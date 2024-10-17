01f58553ea450e87d3e0a27ce37c75c5091cf3b3
bnanupam <bnanupam@gmail.com>
2024-10-17 18:38:03 +0530

    addpara

--- src/App.jsx
+++ src/App.jsx
@@ -74,7 +74,48 @@ function App() {
     </ul>
   
    </div>
+   <div className='secondconts'>
+    <h2>
+    Personal Loan Eligibility
+    </h2>
    
+    <ul class="info-list">
+        <li><strong>Age:</strong> 21 - 65 years</li>
+        <li><strong>Income:</strong> Minimum Rs 15,000/month for salaried applicants</li>
+        <li><strong>Resident:</strong> A resident of India</li>
+    </ul>
+
+   </div>
+   <div className='largcontainer'>
+    <h2>
+    What is a Personal Loan?
+    </h2>
+    <p>
+    A loan is when a party borrows money from another party. A personal loan is a type of unsecured loan provided by financial institutions, including banks, credit unions, and online lenders, which can be used for various personal expenses. These loans do not need collateral, which means, you don't have to provide an asset (like a house) to secure the loan. Personal loans often come with higher interest rates as they are unsecured. Borrowers can use personal loans for a wide range of purposes, making them a flexible financing option.
+    </p>
+    <h2>
+    When to Consider a Personal Loan?
+    </h2>
+   
+      <ul>
+        <li>
+        Emergency Expenses: Personal loans can be a lifesaver in emergency situations where immediate funds are needed, such as unexpected medical bills or urgent home repairs. They provide quick access to cash without the need to liquidate investments or assets.
+        </li>
+        <li>
+        Debt Consolidation: If you have multiple debts with varying interest rates, a personal loan can consolidate these debts into a single payment with a potentially lower interest rate, simplifying your finances and possibly reducing your overall interest payments.
+        </li>
+        <li>
+        Large Purchases: Financing significant expenses, like weddings, vacations, or major appliances, can be more manageable with a personal loan. This allows you to spread the cost over a period of time rather than paying upfront.
+        </li>
+        <li>
+        Home Improvement:Personal loans can fund home renovation or remodeling projects, helping you increase the value of your property without tapping into your home equity.
+        </li>
+        <li>
+        Educational Expenses: While student loans are specifically designed for education costs, a personal loan can cover additional expenses like books, supplies, or living costs if student loans fall short.
+        </li>
+      </ul>
+   
+   </div>
   </footer>
 
     </div>
