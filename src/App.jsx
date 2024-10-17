4c95b90bff7ec71b6d8ff943a1c4b041bb8f7f7e
bnanupam <bnanupam@gmail.com>
2024-10-17 12:12:26 +0530

    imgpathchange

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.scss'
-import Secure from './assets/image/img1.avif'
-import girlimg from './assets/image/bannerimg.avif'
+import Secure from './assets/img1.avif'
+import girlimg from './assets/bannerimg.avif'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -24,13 +22,13 @@ function App() {
             Find Best Personal<br />
             Loan Offers From<br />
             <p style={{ color: 'green', fontSize: 40 }}>10+ Lenders </p>
-            <p className='para'> Get Instant Loan Offers of up to 10 Lakh</p>
+            <p> Get Instant Loan Offers of up to 10 Lakh</p>
           </p>
           <p className='telbox'>
             <input type="tel" pattern="[A-Za-z]+" name="phone" required="" maxlength="14" id="input-box" inputmode="numeric" value="+91 " />
           </p>
           <button class="landingPageCss_button___D_Bd">Get Loan Offers</button>
-          <p>
+          <p className='para'>
             By continuing, I agree to CIBIL Terms and Conditions and<br />
             authorize HTDS Lending Partners to collect and store the<br />
             Credit Bureau data for checking my eligibility for loan.
